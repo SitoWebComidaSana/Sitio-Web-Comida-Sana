266a695910cf0fe0fa9697e541b61724232770f8
erlugarc2 <erwin.garcia.liberio@gmail.com>
2014-07-21 00:26:54 -0500

    cambios
    cambios

---
+++ Desktop/Proyecto/reservatelefonica.php
@@ -0,0 +1,50 @@
+<?php
+    include("includes/top_page.php");
+?>
+<html>
+ <head>
+ </head>
+     
+<body>
+    
+<!--------------Content--------------->
+<section id="content">
+	<div class="wrap-content zerogrid">
+		<div class="row block">
+			<div id="main-content" class="col-full">
+				<div class="col-1-3">
+					<div class="wrap-col">
+						<article>
+							<h2><a href="#">Reserva Telef&oacutenica</a></h2>
+							<!--<img src="images/reservacion1.jpg"/> -->
+							<p> &iexcl &iexcl Es el momento de reservar!
+							
+
+Puede reservar una mesa en el horario siguiente:
+
+ 
+
+ABIERTO TODOS LOS DIAS A PARTIR DE LAS 10:00
+
+ 
+
+Atenderemos gustosamente su petici&oacuten de reserva ya sea por tel&eacutefono o directamente a trav&eacutes de nuestro formulario en l&iacutenea.
+
+ 
+
+Reservas por tel&eacutefono
+
+99999999
+
+
+Reservas a trav&eacutes del <a href="formularioLinea.html"> formulario en l&iacutenea</a> [...]</p>
+						
+					</div>
+				</div>
+				
+</section>
+</body>
+
+
+    
+</html>
--- registro.php
+++ registro.php
@@ -1,11 +1,77 @@
-
-
-
 <?php
-echo <<< HERE
-$username = filter_input(input_POST), "");
-print "hola $username "
-
-
-HERE;
+    include("includes/top_page.php");
 ?>
+<!--------------Header--------------->
+<header>
+<?php include("../NewProject/includes/logo.php"); ?>
+</header>
+<nav>
+	<div class="wrap-nav zerogrid">
+		<?php include("../NewProject/includes/menu.php"); ?>		
+		<?php include("../NewProject/includes/social.php"); ?>
+</nav>
+
+<body>
+Formulario de Registro
+<div  id = "div1" >
+<h1> CREACION DE CUENTA </ h1>
+  <div  id = "div2" >
+    
+	<form action= "login.php" method = "post">
+	<fieldset>
+     <legend> Registrate Y SE PARTE DE NUESTROS CLIENTES EXCLUSIVOS </legend>
+  	 <div class="Datos">	
+     <label> Nombre </label>
+     <input type="text" name="username" id="username"/>
+     </div>
+     <div class="Datos"> 
+     <label for="passwd">Contrase&ntilde;a</label>
+     <input type="password" name="passwd" id="passwd"
+     placeholder="qwe2458"
+     pattern="[a-z] {3} [0-9] {4}"
+     required/> 
+      </div>
+      
+      <div class="Datos"> 
+     <label for="passwd">Confirmar</label>
+     <input type="password" name="passwd" id="passwd"
+     placeholder="qwe2458"
+     pattern="[a-z] {3} [0-9] {4}"
+     required/> 
+      </div>
+      
+     <div class="Datos"> 
+      <label for="phone"> Telefono </label>
+    <input type="telef" phone id="phone" 
+     placeholder="47965453635">
+      </div>
+      
+      <div class="Datos">
+     	 <label for="mail"> Email </label>
+     	<input type="mail" name="mail" id="mail" 
+     	placeholder="ejemplo@gmail.com">
+          </div>
+     
+      <div class="Datos">
+    	 <label for="address"> Direccion Domicilio </label>
+    	 <input type="address" name="address" id="address" 
+      	placeholder="Plaza Dañin y Albatros">
+      </div>
+     </fieldset>
+     <div>
+     <button type="submit">  Registrar  </button>
+	 </div>
+
+    </form>	
+</div>
+</div>
+</body>
+ <br style="clear:both;" />
+
+    </div>
+    <div id="footer">   	   
+	    <?php include("includes/footer.php"); ?>        
+    </div>
+</div>
+<?php include("includes/bottom_page.php"); ?>
+</html>
