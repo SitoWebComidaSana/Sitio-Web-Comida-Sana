79522860d23f007fa5586fa2e5220bdb581c5b79
jinsala <gricelsv@hotmail.com>
2014-08-03 23:38:27 -0500

    Testimonios

---
+++ Testimonio.php
@@ -0,0 +1,87 @@
+<?php
+    include("includes/top_page.php");
+?>
+<!--------------Header--------------->
+<header>
+<?php include("../NewProject/includes/logo.php"); ?>
+</header>
+<nav>
+	<div class="wrap-nav zerogrid">
+		<?php include("../NewProject/includes/menu.php"); ?>		
+		<?php include("../NewProject/includes/social.php"); ?>
+        <p class="linblock">Elemento con comportamiento en línea y en bloque</p>
+<p class="linblock">Otro elemento que se comporta en línea y en bloque</p> 
+
+
+</nav>
+
+		</div> 
+	</div> 
+		</div> 
+	</div> 
+</div><div class="wpb_row vc_row-fluid">
+	<div class="vc_span12 wpb_column column_container">
+		<div class="wpb_wrapper">
+			
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			
+<p style="text-align: center;"><img class="aligncenter size-full wp-image-1095" alt="pic-home" src="/wp-content/uploads/2014/02/pic-home1.jpg" width="714" height="258" /></p>
+
+		</div> 
+	</div> 
+		</div> 
+	</div> 
+</div><div class="wpb_row vc_row-fluid">
+	<div class="vc_span12 wpb_column column_container">
+		<div class="wpb_wrapper">
+			
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			
+<p style="text-align: center;"><img class="aligncenter size-full wp-image-1096" alt="marcas" src="http://thermos.pe/wp-content/uploads/2013/05/marcas.jpg" width="624" height="139" /></p>
+
+		</div> 
+	</div> 
+		</div> 
+	</div> 
+</div>
+       
+<body>
+    
+<!--------------Content--------------->
+<section id="content">
+	<div class="wrap-content zerogrid">
+		<div class="row block">
+			<div id="main-content" class="col-full">
+				<div class="col-1-3">
+					<div class="wrap-col">
+						<article>
+							<h2><a href="#">Testimonios</a></h2>
+							
+					</div>
+				</div>
+				<div class="col-1-3">
+					<div class="wrap-col">
+						<article>
+							<h2><a href="#">Servicios a Domicilio</a></h2>
+							<img src="images/servicesdomicilio.jpg"/>
+                            
+
+					</div>
+				</div>
+				
+</section>
+</body>
+
+
+        
+        <br style="clear:both;" />
+
+    </div>
+    <div id="footer">   	   
+	    <?php include("includes/footer.php"); ?>        
+    </div>
+</div>
+<?php include("includes/bottom_page.php"); ?>
+</html>
