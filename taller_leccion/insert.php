d445c02133a8432da968d41d69876a1bd90a7f9e
Jinna <gricelsv@hotmail.com>
2014-08-26 20:53:04 -0500

    Update insert.php

--- taller_leccion/insert.php
+++ taller_leccion/insert.php
@@ -1,15 +1,28 @@
+<html>
+<head>
+</head>
+<body>
+<div id="main">
+<?php
 <?php
 $valor1=$_POST ("nombre");
 $valor2=$_POST ("descripcion");
 $valor3=$_POST ("imagen");
 
-incule_once ("DemoCollector.php");
-
-DemoCollectorObj= new DemoCollector();
-DemoCollectorObj= new createPlato($valor1, $valor2, $valor3);
+echo 'Hola ' . htmlspecialchars($valor1) . ($valo2)  . '!';
+echo 'ruta archivo ' . htmlspecialchars($valor3) . '!';
+include_once ("PlatoCollector1.php");
+ 
+PlatosCollectorObj= new PlatoCollector1();
+PlatosCollectorObj->createPlato($valor1, $valor2, $valor3);
 
 echo "valor agregado";
 
 
 ?>
+
+<div><a href="index.php">Volver al Inicio</a></div>
+</div>
+</body>
+</html>
 
